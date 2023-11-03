2507364bc319799642d9fb5545a1c51918da76ff
Afonso Figuinha <afonso.figuinha@gmail.com>
2023-11-03 21:19:27 +0000

    Products code readability

--- src/lib/invoice-generator/billptinvoice/modules/product.ts
+++ src/lib/invoice-generator/billptinvoice/modules/product.ts
@@ -2,10 +2,23 @@ import { API_ITEMS, urlSafe } from "./utils";
 
 export async function getProductURLString(prod: any, idx: number = 0) {
   const prodName = urlSafe(prod.productName + " - " + prod.variantName, 200);
-  const iva = prod.taxRate === 13 ? "13577" : "13576"; //13% -> 13577; 23% -> 13576; 0->0
-  const price = prod.unitPrice.net.amount; //preco unitario sem iva e com descontos
+
+  const iva = (() => {
+    //13% -> 13577; 23% -> 13576; 0->0
+    switch (prod.taxRate) {
+      case 13:
+        return "13577";
+      case 0:
+        return "0";
+      default:
+        return "13576";
+    }
+  })();
+
+  const price = prod.unitPrice.net.amount; //unit price without vat with discounts
   const prodSaleorId: string = urlSafe(prod.productVariantId ?? prodName, 100);
   const findProdUrl = `${API_ITEMS}&pesquisa[texto][codigo_barras]=${prodSaleorId}`;
+
   let prodId = "";
 
   await fetch(findProdUrl)
@@ -17,7 +30,17 @@ export async function getProductURLString(prod: any, idx: number = 0) {
     });
 
   if (prodId === "") {
-    const createProdUrl = `${API_ITEMS}&descricao=${prodName}&unidade_medida_id=16094&ProductCategory=M&movimenta_stock=0&servico=0&iva_compra=0&imposto_id=${iva}&codigo_barras=${prodSaleorId}&precos[0][preco_nome]=Price1&precos[0][preco_sem_iva]=${price}`;
+    let createProdUrl = API_ITEMS;
+    createProdUrl += `&descricao=${prodName}`;
+    createProdUrl += `&unidade_medida_id=16094`;
+    createProdUrl += `&ProductCategory=M`;
+    createProdUrl += `&movimenta_stock=0`;
+    createProdUrl += `&servico=0`;
+    createProdUrl += `&iva_compra=0`;
+    createProdUrl += `&imposto_id=${iva}`;
+    createProdUrl += `&codigo_barras=${prodSaleorId}`;
+    createProdUrl += `&precos[0][preco_nome]=Price1`;
+    createProdUrl += `&precos[0][preco_sem_iva]=${price}`;
 
     await fetch(createProdUrl, { method: "POST" })
       .then((response) => response.json())
@@ -28,6 +51,11 @@ export async function getProductURLString(prod: any, idx: number = 0) {
 
   console.log("Product ID: " + prodId);
 
-  //default was: &produtos[0][item_id]=${prodId}&produtos[0][nome]=Saroto Tinto 2018&produtos[0][quantidade]=1&produtos[0][preco_unitario]=40.00&produtos[0][imposto]=13
-  return `&produtos[${idx}][item_id]=${prodId}&produtos[${idx}][nome]=${prodName}&produtos[${idx}][quantidade]=${prod.quantity}&produtos[${idx}][preco_unitario]=${price}&produtos[${idx}][imposto_id]=${iva}`;
+  let prodString = `&produtos[${idx}][item_id]=${prodId}`;
+  prodString += `&produtos[${idx}][nome]=${prodName}`;
+  prodString += `&produtos[${idx}][quantidade]=${prod.quantity}`;
+  prodString += `&produtos[${idx}][preco_unitario]=${price}`;
+  prodString += `&produtos[${idx}][imposto_id]=${iva}`;
+
+  return prodString;
 }
